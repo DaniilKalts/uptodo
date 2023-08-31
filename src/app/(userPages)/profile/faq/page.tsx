541fbb8b9fbacd6bf76fb8f34b461890b5f7db57
DaniilKalts <109500182+DaniilKalts@users.noreply.github.com>
2023-08-31 14:49:53 +0600

    Improved UI for Faq page

--- src/app/(userPages)/profile/faq/page.tsx
+++ src/app/(userPages)/profile/faq/page.tsx
@@ -64,28 +64,6 @@ const Faq = () => (
                   clipRule="evenodd"
                 ></path>
               </svg>
-              Can I set reminders for my tasks?
-            </h3>
-            <p className="text-sm text-gray-500 dark:text-white-pale min-[475px]:text-base">
-              Yes, you can set reminders for your tasks in UpTodo. When creating
-              or editing a task, you can specify a due date and time, and UpTodo
-              will automatically remind you when the task is due.
-            </p>
-          </div>
-          <div className="mb-10">
-            <h3 className="mb-4 flex items-center text-lg font-bold text-gray-900 dark:text-white min-[475px]:text-2xl">
-              <svg
-                className="mr-2 h-6 w-6 flex-shrink-0 text-gray-500 dark:text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
               How do I organize my tasks in UpTodo?
             </h3>
             <p className="text-sm text-gray-500 dark:text-white-pale min-[475px]:text-base">
@@ -115,6 +93,27 @@ const Faq = () => (
               Currently, UpTodo does not support direct task sharing with
               others. However, you can export task lists or share screenshots to
               collaborate on tasks with external parties.
+            </p>
+          </div>
+          <div className="mb-10">
+            <h3 className="mb-4 flex items-center text-lg font-bold text-gray-900 dark:text-white min-[475px]:text-2xl">
+              <svg
+                className="mr-2 h-6 w-6 flex-shrink-0 text-gray-500 dark:text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              Can I access UpTodo on different devices?
+            </h3>
+            <p className="text-sm text-gray-500 dark:text-white-pale min-[475px]:text-base">
+              Yes, UpTodo is designed to be accessible across web browsers.
+              Therefore, you can access wherever you want to.
             </p>
           </div>
         </div>
@@ -186,27 +185,6 @@ const Faq = () => (
               usually find a "Feedback" or "Help" section within the app or on
               the website. There, you'll find contact details, email support
               option to reach out for help.
-            </p>
-          </div>
-          <div className="mb-10">
-            <h3 className="mb-4 flex items-center text-lg font-bold text-gray-900 dark:text-white min-[475px]:text-2xl">
-              <svg
-                className="mr-2 h-6 w-6 flex-shrink-0 text-gray-500 dark:text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              Can I access UpTodo on different devices?
-            </h3>
-            <p className="text-sm text-gray-500 dark:text-white-pale min-[475px]:text-base">
-              Yes, UpTodo is designed to be accessible across web browsers.
-              Therefore, you can access wherever you want to.
             </p>
           </div>
         </div>
