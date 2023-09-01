898609ee970489a3669368628ec2c905c26f7248
DaniilKalts <109500182+DaniilKalts@users.noreply.github.com>
2023-09-01 21:14:01 +0600

    Changed Loading Structure & Fixed some small UI problems

--- src/app/(userPages)/calendar/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import CalendarSkeleton from '@/components/userPages/Calendar/CalendarSkeleton';
-
-export default function Loading() {
-  return <CalendarSkeleton />;
-}
