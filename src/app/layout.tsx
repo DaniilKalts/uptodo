de7f904124947977a62618a824677ac862ef1ce5
DaniilKalts <109500182+DaniilKalts@users.noreply.github.com>
2023-09-06 07:48:36 +0600

    Attempt to Fix Font families issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,10 @@ import './globals.css';
 import React from 'react';
 
 import { Metadata } from 'next';
-import { Lato } from 'next/font/google';
 
-import ToasterProvider from '@/providers/ToasterProvider';
 import { ThemeProvider } from '@/providers/ThemeProvider';
-
-import { cookies } from 'next/headers';
-import { typographyDemos } from '@/utils/Typografies';
-
-const lato = Lato({
-  subsets: ['latin'],
-  weight: ['300', '400', '700'],
-});
+import ToasterProvider from '@/providers/ToasterProvider';
+import FontProvider from '@/providers/FontProvider';
 
 export const metadata: Metadata = {
   title: 'UpTodo 2023',
@@ -26,23 +18,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = cookies();
-  const typographyName = cookieStore.get('typographyName')?.value;
-
-  const appFont = typographyDemos.find(
-    (demo) => demo.name === typographyName,
-  )?.styleName;
-
   return (
     <html lang="en">
-      <body
-        className={`${
-          appFont || lato.className
-        } bg-white-pale dark:bg-black-pre`}
-      >
+      <body className={'bg-white-pale dark:bg-black-pre'}>
         <ThemeProvider attribute="class" defaultTheme="dark">
-          <ToasterProvider />
-          {children}
+          <FontProvider>
+            <ToasterProvider />
+            {children}
+          </FontProvider>
         </ThemeProvider>
       </body>
     </html>
---
+++ src/providers/FontProvider.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable import/prefer-default-export */
+
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { Lato } from 'next/font/google';
+
+import { typographyDemos } from '@/utils/Typografies';
+
+import { useCookies } from 'react-cookie';
+
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+});
+
+const FontProvider = ({ children }: { children: React.ReactNode }) => {
+  const [cookies] = useCookies(['typographyName']);
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  const typographyStyles = typographyDemos.find(
+    (demo) => demo.name === cookies?.typographyName,
+  )?.styleName;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMounted(true);
+    }, 500);
+  }, []);
+
+  return (
+    <div>
+      {mounted ? (
+        <div
+          className={
+            mounted ? typographyStyles || lato.className : lato.className
+          }
+        >
+          {children}
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default FontProvider;
